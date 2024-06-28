39d2162f3b8946ae0e63ce69bbc11af81193a26f
Kunal Mishra <48254970+kunalm2345@users.noreply.github.com>
2024-06-28 23:18:53 +0530

    first of thank you random stack overflow user who made dumb me able to use vercel now finally this works. even with mobile phones

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+      '32': '32px',
+    },
     extend: {},
   },
   plugins: [],

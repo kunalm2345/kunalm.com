e3e115a8adc45d599c68672ca044378b13872b11
Kunal Mishra <48254970+kunalm2345@users.noreply.github.com>
2024-06-30 23:01:30 +0530

    fixed navbar

--- src/.svelte-kit/generated/client/nodes/0.js
+++ src/.svelte-kit/generated/client/nodes/0.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 export { default as component } from "../../../../../node_modules/@sveltejs/kit/src/runtime/components/layout.svelte";
+=======
+export { default as component } from "../../../../node_modules/@sveltejs/kit/src/runtime/components/layout.svelte";
+>>>>>>> 8f34d47 (fixed navbar)
--- src/.svelte-kit/generated/client/nodes/1.js
+++ src/.svelte-kit/generated/client/nodes/1.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 export { default as component } from "../../../../../node_modules/@sveltejs/kit/src/runtime/components/error.svelte";
+=======
+export { default as component } from "../../../../node_modules/@sveltejs/kit/src/runtime/components/error.svelte";
+>>>>>>> 8f34d47 (fixed navbar)

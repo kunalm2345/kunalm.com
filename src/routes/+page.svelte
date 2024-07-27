5406dd687669c3ed27ba474c480f4c13f92c8aba
Kunal Mishra <48254970+kunalm2345@users.noreply.github.com>
2024-07-27 17:55:11 +0530

    minor copy edits and font change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 <p class="text-2xl">
     Hi! I'm Kunal, an electronics major at BITS, Goa.
     <br><br>
-    Right now, I'm interning at <a href="http://biocomputeinc.com">BioCompute, Inc</a> for computational research and working in <a href="http://arnabkrpaul.github.io/Dashlab/">DaSH lab</a> under Prof Arnab Paul among a few other engagements. 
+    Right now, I'm working at building features at <a href="http://buttondown.email">Buttondown</a>. Before that, I did computational research at <a href="https://biocomputeinc.com">BioCompute, Inc</a> and working in <a href="http://arnabkrpaul.github.io/Dashlab/">DaSH lab</a> under Prof Arnab Paul among a few other engagements. 
     <br><br>
 
     I fell in love with computers and the Internet when I was 10. 

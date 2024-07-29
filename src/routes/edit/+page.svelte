261fe0ab8b9e7bb2d45d788d564ffb204aab9486
Kunal Mishra <48254970+kunalm2345@users.noreply.github.com>
2024-07-29 21:02:57 +0530

    added writing dash for /work

---
+++ src/routes/edit/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { workPage } from '../../store';
+    let content = '';
+    workPage.subscribe(val => {
+        content = val;
+    });
+
+    function updateContent() {
+        workPage.set(content);
+        alert("done");
+    }
+</script>
+
+<h1 class="font-serif text-6xl">Edit Work</h1>
+
+<form on:submit|preventDefault={updateContent}>
+    <textarea bind:value={content} class="w-full h-lvh"></textarea>
+    <button type="submit">Update HTML Content</button>
+</form>
--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -1,25 +1,9 @@
-<h1 class="font-serif text-6xl mb-2">Work Experience</h1>
-<p class="text-2xl">
-    Currently I'm working on: <br><br>
-</p>
-<ul class="text-2xl list-inside no-single-line-list">
-   
-    <li><b>Research Intern at BioCompute, Inc</b>
-        <br>
+<script>
+    import { workPage } from '../../store';
+    let content = '';
+    workPage.subscribe(val => {
+        content = val;
+    });
+</script>
 
-        Bacterial DNA can store digital data thousands of times more densely than Seagate's current best tech. We're working on a device which would bring this in a form you can plug into your PC. 
-    </li> <br>
-
-    <li><b>Student Researcher at DaSH Lab</b> – I'm working under Prof Arnab Paul on building data prefetching for distributed ML training on resource constrained devices.    </li>
-    
-</ul>
-<p class="text-2xl"><br>Previously: <br><br> </p>
-<ul class="text-2xl list-inside no-underline">
-   
-    <li><b>SEO Lead at HelloMeets</b> — I handled the entire publishing process for the HelloMeets blog while growing it from 600 to over 45k visits a month, in 8 months on a strict budget.
-    </li> <br>
-
-    <li><b>Student Researcher at DaSH Lab</b> – I'm working under Prof Arnab Paul on building data prefetching for distributed ML training on resource constrained devices.    </li>
-    
-</ul>
-
+{@html content}

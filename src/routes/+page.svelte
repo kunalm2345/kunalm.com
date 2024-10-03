2affa5f39db4e41615dec993de3093fe232dc18a
Kunal Mishra <48254970+kunalm2345@users.noreply.github.com>
2024-10-04 02:32:49 +0530

    updates of home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <h1 class="font-serif text-6xl mb-2">Kunal Mishra</h1>
 <p class="text-2xl">
+    <br><br>
     Hi! I'm Kunal, an electronics major at BITS, Goa.
     <br><br>
-    Right now, I'm working at building features at <a href="http://buttondown.email">Buttondown</a>. Before that, I did computational research at <a href="https://biocomputeinc.com">BioCompute, Inc</a> and working in <a href="http://arnabkrpaul.github.io/Dashlab/">DaSH lab</a> under Prof Arnab Paul among a few other engagements. 
+    Right now, I'm part-time building new features at <a href="http://buttondown.email">Buttondown</a>, working on a research project on smart spaces at <a href="http://arnabkrpaul.github.io/Dashlab/">DaSH lab</a> and maintaining my indie-saas, <a href="https://socialscri.be/">socialscri.be</a>.
+    <br><br>
+    Before that, I did computational research at <a href="https://biocomputeinc.com">BioCompute, Inc</a> (they got WTFund'ed).
     <br><br>
 
     I fell in love with computers and the Internet when I was 10. 
-    Ever since, I have written a few newsletters, did content marketing
-    and SEO for startups and built a bunch of web-apps that earned me some
+    Ever since, I have written a few newsletters, did SEO, designing and consultant-work for startups and built a bunch of saas products that earned me some
     pocket money. 
     <br><br>
     

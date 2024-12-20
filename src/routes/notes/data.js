bd5afbdd27bd244cfa21ad84ac9b604faf602c25
Kunal Mishra <48254970+kunalm2345@users.noreply.github.com>
2024-12-21 02:22:31 +0530

    new note

--- src/routes/notes/data.js
+++ src/routes/notes/data.js
@@ -1,68 +1,160 @@
 export const posts = [
-// 	{
-// 		slug: 'welcome',
-// 		title: 'Welcome to the Aperture Science computer-aided enrichment center',
-// 		content:
-// 			'<p>We hope your brief detention in the relaxation vault has been a pleasant one.</p><p>Your specimen has been processed and we are now ready to begin the test proper.</p>'
-// 	},
+	{
+		slug: 'algebra-of-happiness',
+		title: 'Algebra of Happiness by Scott Galloway',
+		content: `
+			<ul class="list-decimal space-y-4 note">
+        <li>Introduction</li>
+        <ul>
+          <li>Work while you're young</li>
+          <ul>
+            <li>
+              <blockquote>
+                <p>if balance is your priority in your youth, then you need to accept that, unless you are a genius, you may not reach the upper rungs of economic security.<br/></p>
+              </blockquote>
+            </li>
+            <li>
+              <blockquote>
+                <p>The slope of the trajectory for your career is (unfairly) set in the first five years post-graduation. If you want the trajectory to be steep, you’ll need to burn a lot of fuel. The world is not yours for the taking, but for the trying. Try hard, really hard.<br/></p>
+              </blockquote>
+            </li>
+            <li>4-5 years of extremity is necessary to do something great — Alakh Pandey said something like in his podcast with Varun Mayya. This is a very generally accepted idea: to be in the "upper rungs" of economic safety, at one point in your life—preferably early on—you need to work extremely hard. To points of insanity.</li>
+            <li>If you crave balance, it's totally alright. But you have to be okay with not being at the top. It's a fair price to pay. And it's very imp to judge what you want ambition or balance. (This might also be a good criterion to categories people on: balance seekers and ambition seekers)</li>
+            <li>
+              <blockquote>
+                <p>I have a lot of balance now. It’s a function of the lack of it in my twenties and thirties. From twenty-two to thirty-four, other than business school, I remember work and not much else. The world does not belong to the big, but to the fast. You want to cover more ground in less time than your peers. This is partially built on talent, but mostly on strategy and endurance. My lack of balance as a young professional cost me my marriage, my hair, and arguably my twenties. There’s no user manual here, and it’s a trade-off. My lack of balance, while affording me more balance later in life, came at a very real cost.<br/></p>
+              </blockquote>
+            </li>
+            <li>You can get balance later in your life, but you will lose your 20s. And some potential memories along with it.</li>
+            <li/>
+          </ul>
+          <li>
+            <blockquote>
+              <p>The ratio of time you spend sweating to watching others sweat is a forward-looking indicator of your success.<br/></p>
+            </blockquote>
+          </li>
+          <ul>
+            <li>sweating = working, sports, doing, etc</li>
+          </ul>
+          <li>Choosing a spouse</li>
+          <ul>
+            <li>Spouse and partner</li>
+            <ul>
+              <li>
+                <blockquote>
+                  <p>Misalignment on what’s important and a lack of appreciation for the other person makes everything . . . harder.<br/></p>
+                </blockquote>
+              </li>
+              <li>
+                <blockquote>
+                  <p>My friends with less economic success who spend less time with friends but who have a real partner to share their struggles and successes with are tangibly happier.<br/></p>
+                </blockquote>
+              </li>
+            </ul>
+            <li>Passion, Values, Money</li>
+            <ul>
+              <li>
+                <blockquote>
+                  <p>The best romantic partnerships I know of are synced up on three things. They are physically attracted to each other. Sex and affection establish your relationship as singular and say “I choose you” nonverbally. Good sex is 10 percent of a relationship, but bad sex is 90 percent of a relationship. However, this is where most young people end their due diligence. You also need to ensure that you align on values like religion, how many kids you want, your approaches to raising kids, your proximity to your parents, sacrifices you’re willing to make for economic success, and who handles which responsibilities. Money is an especially important value for alignment, as the number one source of marital acrimony is financial stress. Does your partner’s contribution to, approach to, and expectations about money—and how it flows in and out of the household—fit with yours?<br/></p>
+                </blockquote>
+              </li>
+            </ul>
+          </ul>
+          <li>Money can buy happiness, to a point.</li>
+          <ul>
+            <li>
+              <blockquote>
+                <p>So, yes, work your ass off and get some semblance of economic stability. But take notes on the things that give you joy and satisfaction, and start investing in those things. Pay special attention to things that bring you joy that don’t involve mind-altering substances or a lot of money.<br/></p>
+              </blockquote>
+            </li>
+          </ul>
+          <li>Harness the power of compounding in not only money but also memories and everything else. </li>
+          <li>No alcohol. Period.</li>
+          <ul>
+            <li>
+              <blockquote>
+                <p>The presence of one thing in a man’s life predicted unhappiness better than any other factor: alcohol. It led to failed marriages, careers coming off the tracks, and bad health.<br/></p>
+              </blockquote>
+            </li>
+            <li>
+              <blockquote>
+                <p>Ask yourself, post-college, if substances are getting in the way of your relationships, professional trajectory, or life. If they are, address it.<br/></p>
+              </blockquote>
+            </li>
+          </ul>
+          <li>providing comfort for someone you love at the end of their life is also deeply satisfying</li>
+          <ul>
+            <li>
+              <blockquote>
+                <p>If you’re in a position—and many aren’t—to make a loved one’s exit more graceful, do it—you’ll cherish the experience for the rest of your life.<br/></p>
+              </blockquote>
+            </li>
+            <li>comes off to me like he's trying to justify his decision</li>
+          </ul>
+          <li>The key to success is the ability to mourn and then move on</li>
+          <li>Nothing Is Ever As Bad or As Good As It Seems</li>
+        </ul>
+</ul>
+      `
+	}
 
-// 	{
-// 		slug: 'safety',
-// 		title: 'Safety notice',
-// 		content:
-// 			'<p>While safety is one of many Enrichment Center Goals, the Aperture Science High Energy Pellet, seen to the left of the chamber, can and has caused permanent disabilities, such as vaporization. Please be careful.</p>'
-// 	},
+	// 	{
+	// 		slug: 'safety',
+	// 		title: 'Safety notice',
+	// 		content:
+	// 			'<p>While safety is one of many Enrichment Center Goals, the Aperture Science High Energy Pellet, seen to the left of the chamber, can and has caused permanent disabilities, such as vaporization. Please be careful.</p>'
+	// 	},
 
-// 	{
-// 		slug: 'getting-real',
-// 		title: 'Getting Real by Basecamp',
-// 		content: `
-// 		<ul>
-// <li>
-// Don't build a company that follows competitors. If they add 10 features, you don't need to add 20. Don't be defensive and paranoid.
-// </li>
-// <li>
-// <h3>What's your problem</h3>
-// <p>Great software solves your own problems, is born out of necessity and you care about it.</p>
-// <ul>
-// <li>
-// <p>A great way to build software is to start out by solving your own problems. You’ll be the target audience and you’ll know what’s important and what’s not. That gives you a great head start on delivering a breakout product.  —Malcolm Gladwell, author (from A Few Thin Slices of Malcolm Gladwell)</p>
-// </li>
-// </ul>
-// </li>
-// <li>
-// <h3>Outside money is Plan B</h3>
-// <p>Investors slow down the process. Besides you don't need money anyways. Money constrains also force creativity and innovation. Which is a good thing.</p>
-// <ul>
-// <li>
-// <p>[Jake Walker started one company with investor money (Disclive) and one without (The Show). Here he discusses the differences between the two paths.] The root of all the problems wasn’t raising money itself, but everything that came along with it. The expectations are simply higher. People start taking salary, and the motivation is to build it up and sell it, or find some other way for the initial investors to make their money back. In the case of the first company, we simply started acting much bigger than we were — out of necessity… [With The Show] we realized that we could deliver a much better product with less costs, only with more time. And we gambled with a bit of our own money that people would be willing to wait for quality over speed. But the company has stayed (and will likely continue to be) a small operation. And ever since that first project, we’ve been fully self funded. With just a bit of creative terms from our vendors, we’ve never really needed to put much of our own money into the operation at all. And the expectation isn’t to grow and sell, but to grow for the sake of growth and to continue to benefit from it financially.  —A comment from Signal vs. Noise</p>
-// </li>
-// </ul>
-// </li>
-// <li>
-// <h3>Fix time and budget. If you can't deliver, lower the scope. This helps you prioritise what really matters. If you fix all 3, what you deliver will have low levels of quality.</h3>
-// <ul>
-// <li>
-// <p>Scope down. It’s better to make half a product than a half-assed product (more on this later).</p>
-// </li>
-// </ul>
-// </li>
-// <li>
-// <h3>Have a competitor and sell a different story. Don't sell the story of faster performance when your enemy is doing the same, sell the story of a cheaper price.</h3>
-// <ul>
-// <li>
-// <p>Have an enemy. Sometimes the best way to know what your app should be is to know what it shouldn’t be. Figure out your app’s enemy and you’ll shine a light on where you need to go.</p>
-// </li>
-// <li>
-// <p>Choose a big competitor. And do what they don't. Often the best way to describe yourself is through a competitor.</p>
-// </li>
-// <li>
-// <p>Take a different angle. Don't sell a story of faster performance when the competition is doing the same, sell the story of a cheaper price. And make people realise your story matters more.  <br>
-// And usually you wanna avoid taking lower pricing as your differentiating angle. It's a race to the bottom.</p>
-// </li>
-// </ul>
-// </li>
-// <li>
-// <p>If your app doesn’t excite you, something’s wrong. If you’re only working on it in order to cash out, it will show. Likewise, if you feel passionately about your app, it will come through in the final product. People can read between the lines.</p>`,
-// 	}
+	// 	{
+	// 		slug: 'getting-real',
+	// 		title: 'Getting Real by Basecamp',
+	// 		content: `
+	// 		<ul>
+	// <li>
+	// Don't build a company that follows competitors. If they add 10 features, you don't need to add 20. Don't be defensive and paranoid.
+	// </li>
+	// <li>
+	// <h3>What's your problem</h3>
+	// <p>Great software solves your own problems, is born out of necessity and you care about it.</p>
+	// <ul>
+	// <li>
+	// <p>A great way to build software is to start out by solving your own problems. You’ll be the target audience and you’ll know what’s important and what’s not. That gives you a great head start on delivering a breakout product.  —Malcolm Gladwell, author (from A Few Thin Slices of Malcolm Gladwell)</p>
+	// </li>
+	// </ul>
+	// </li>
+	// <li>
+	// <h3>Outside money is Plan B</h3>
+	// <p>Investors slow down the process. Besides you don't need money anyways. Money constrains also force creativity and innovation. Which is a good thing.</p>
+	// <ul>
+	// <li>
+	// <p>[Jake Walker started one company with investor money (Disclive) and one without (The Show). Here he discusses the differences between the two paths.] The root of all the problems wasn’t raising money itself, but everything that came along with it. The expectations are simply higher. People start taking salary, and the motivation is to build it up and sell it, or find some other way for the initial investors to make their money back. In the case of the first company, we simply started acting much bigger than we were — out of necessity… [With The Show] we realized that we could deliver a much better product with less costs, only with more time. And we gambled with a bit of our own money that people would be willing to wait for quality over speed. But the company has stayed (and will likely continue to be) a small operation. And ever since that first project, we’ve been fully self funded. With just a bit of creative terms from our vendors, we’ve never really needed to put much of our own money into the operation at all. And the expectation isn’t to grow and sell, but to grow for the sake of growth and to continue to benefit from it financially.  —A comment from Signal vs. Noise</p>
+	// </li>
+	// </ul>
+	// </li>
+	// <li>
+	// <h3>Fix time and budget. If you can't deliver, lower the scope. This helps you prioritise what really matters. If you fix all 3, what you deliver will have low levels of quality.</h3>
+	// <ul>
+	// <li>
+	// <p>Scope down. It’s better to make half a product than a half-assed product (more on this later).</p>
+	// </li>
+	// </ul>
+	// </li>
+	// <li>
+	// <h3>Have a competitor and sell a different story. Don't sell the story of faster performance when your enemy is doing the same, sell the story of a cheaper price.</h3>
+	// <ul>
+	// <li>
+	// <p>Have an enemy. Sometimes the best way to know what your app should be is to know what it shouldn’t be. Figure out your app’s enemy and you’ll shine a light on where you need to go.</p>
+	// </li>
+	// <li>
+	// <p>Choose a big competitor. And do what they don't. Often the best way to describe yourself is through a competitor.</p>
+	// </li>
+	// <li>
+	// <p>Take a different angle. Don't sell a story of faster performance when the competition is doing the same, sell the story of a cheaper price. And make people realise your story matters more.  <br>
+	// And usually you wanna avoid taking lower pricing as your differentiating angle. It's a race to the bottom.</p>
+	// </li>
+	// </ul>
+	// </li>
+	// <li>
+	// <p>If your app doesn’t excite you, something’s wrong. If you’re only working on it in order to cash out, it will show. Likewise, if you feel passionately about your app, it will come through in the final product. People can read between the lines.</p>`,
+	// 	}
 ];
